ee9bfcb6e18aa3440578ab5ae0b2f4c9de33944f
RODGERS SSENYONJO <rogerssenyonjo04@gmail.com>
2025-09-03 12:15:30 +0300

    Add files via upload
    C++ is fun

---
+++ Atwooki.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<string.h>
+#include<cmath>
+//#include <cstdlib>
+using namespace std;
+int main()
+{
+    int number = rand();
+    cout<<number<<endl;
+/*char x;
+char w ='P';
+double  b, l,j;
+
+string First_Name, Last_Name;
+
+double m;
+ cout<<w<<endl;
+ cout<<"Enter your name:";
+cin>>First_Name;
+cin>>Last_Name;
+cout <<"My name is " << First_Name<<" " <<Last_Name<< endl;
+cout<<"Enter a character:"<<endl;
+cin>> x;
+cout<< "Character entered is " <<x<<endl;
+cout<<"Enter two numbers below 10:"<<endl;
+ cin>>b>>l;
+j= pow(b,l);
+cout<<"The power of the two numbers entered is " << j<<endl;
+cout<<"Enter a number:"<< endl;
+cin >> m;
+cout <<"Number is " << m << endl;*/
+/*const double pi =3.14;
+//int a=2;
+//int b=3;
+//int temp=a;
+double radius, Area, Circumference;
+//a=b;
+//b=temp;
+//cout<<a<<endl;
+//cout<<b<<endl;
+cout<<"Enter radius of the circle";
+cin>>radius;
+Area = pi*pow(radius, 2);
+Circumference = 2*pi*radius;
+cout<<"Area of the Circle is "<<Area<<endl;
+cout<<"Circumference of the Circle is "<<Circumference<<endl;*/
+double x,y,z;
+cin>>x>>y;
+z=(x+10)/(3*y);
+cout<<"z is "<<z<<endl;
+
+return 0;
+}
