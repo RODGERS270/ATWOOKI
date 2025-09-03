c90f630f13c7e399865dfc8db86c17b288e6a35e
RODGERS SSENYONJO <129655762+RODGERS270@users.noreply.github.com>
2025-09-03 13:53:44 +0300

    Bintamye

--- my_first_program_in_cpp.cpp
+++ my_first_program_in_cpp.cpp
@@ -8,5 +8,6 @@ int main() {
     cout << "Please enter your two names: ";
     getline(cin, fullName); // Reads full line including spaces
     cout << "You entered: " << fullName << endl;
+    cout<<"Hello World!" <<endl;
     return 0;
 }
