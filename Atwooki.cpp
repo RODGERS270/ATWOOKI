073a2f0702789773d1ef240d1bce2427dc45bc81
RODGERS SSENYONJO <129655762+RODGERS270@users.noreply.github.com>
2025-09-03 12:43:21 +0300

    Atwooki
    Please enter the commit message for your changes. Lines starting

--- Atwooki.cpp
+++ Atwooki.cpp
@@ -1,53 +1,63 @@
-#include<iostream>
-#include<string.h>
-#include<cmath>
-//#include <cstdlib>
+#include <iostream>
+#include <cmath>
 using namespace std;
-int main()
-{
+
+int main() {
+    // Print a random number
     int number = rand();
-    cout<<number<<endl;
-/*char x;
-char w ='P';
-double  b, l,j;
+    cout << "Random number: " << number << endl;
 
-string First_Name, Last_Name;
+    // Print a character
+    char w = 'P';
+    cout << w << endl;
 
-double m;
- cout<<w<<endl;
- cout<<"Enter your name:";
-cin>>First_Name;
-cin>>Last_Name;
-cout <<"My name is " << First_Name<<" " <<Last_Name<< endl;
-cout<<"Enter a character:"<<endl;
-cin>> x;
-cout<< "Character entered is " <<x<<endl;
-cout<<"Enter two numbers below 10:"<<endl;
- cin>>b>>l;
-j= pow(b,l);
-cout<<"The power of the two numbers entered is " << j<<endl;
-cout<<"Enter a number:"<< endl;
-cin >> m;
-cout <<"Number is " << m << endl;*/
-/*const double pi =3.14;
-//int a=2;
-//int b=3;
-//int temp=a;
-double radius, Area, Circumference;
-//a=b;
-//b=temp;
-//cout<<a<<endl;
-//cout<<b<<endl;
-cout<<"Enter radius of the circle";
-cin>>radius;
-Area = pi*pow(radius, 2);
-Circumference = 2*pi*radius;
-cout<<"Area of the Circle is "<<Area<<endl;
-cout<<"Circumference of the Circle is "<<Circumference<<endl;*/
-double x,y,z;
-cin>>x>>y;
-z=(x+10)/(3*y);
-cout<<"z is "<<z<<endl;
+    // Get user's name
+    string First_Name, Last_Name;
+    cout << "Enter your first and last name: ";
+    cin >> First_Name >> Last_Name;
+    cout << "My name is " << First_Name << " " << Last_Name << endl;
 
-return 0;
+    // Get a character from user
+    char x;
+    cout << "Enter a character: ";
+    cin >> x;
+    cout << "Character entered is " << x << endl;
+
+    // Power calculation
+    double b, l;
+    cout << "Enter two numbers below 10: ";
+    cin >> b >> l;
+    double j = pow(b, l);
+    cout << "The power of the two numbers entered is " << j << endl;
+
+    // Get a number and print it
+    double m;
+    cout << "Enter a number: ";
+    cin >> m;
+    cout << "Number is " << m << endl;
+
+    // Swap two integers and print them
+    int a = 2, b2 = 3, temp = a;
+    a = b2;
+    b2 = temp;
+    cout << "After swapping: a = " << a << ", b = " << b2 << endl;
+
+    // Circle area and circumference
+    const double pi = 3.14;
+    double radius, Area, Circumference;
+    cout << "Enter radius of the circle: ";
+    cin >> radius;
+    Area = pi * pow(radius, 2);
+    Circumference = 2 * pi * radius;
+    cout << "Area of the Circle is " << Area << endl;
+    cout << "Circumference of the Circle is " << Circumference << endl;
+
+    // Final calculation
+    double x2, y2, z;
+    cout << "Enter two numbers: ";
+    cin >> x2 >> y2;
+    z = (x2 + 10) / (3 * y2);
+    cout << "z is " << z << endl;
+
+    return 0;
 }
